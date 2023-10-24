c20e035a60d87e442efa7a98e430c79ab7fcf830
Michal Kabza <michal.kabza@contractors.roche.com>
2023-10-23 17:18:54 -0700

    Updated the mouse_E18_analysis/ directory

--- mouse_E18_analysis/download_data.sh
+++ mouse_E18_analysis/download_data.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
 # Download the input data for the analysis
-wget https://zenodo.org/record/0000000/files/data_mouse_E18_analysis.tgz
+wget https://zenodo.org/record/10028909/files/data_mouse_E18_analysis.tgz
 tar xfz data_mouse_E18_analysis.tgz
 rm -f data_mouse_E18_analysis.tgz
