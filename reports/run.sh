d2b08144cc3aefd2699841f775a6460bb0a528fe
Michal Kabza <michal.kabza@contractors.roche.com>
2023-11-01 15:52:42 -0700

    Updated the reports/ directory

--- reports/run.sh
+++ reports/run.sh
@@ -6,10 +6,10 @@ singularity exec ../singularity/reports_python.sif \
   --to notebook --inplace simulated_bulk_benchmarks.ipynb
 
 # Build the R reports
-singularity exec ../singularity/reports_r.sif Rscript -e 'rmarkdown::render("simulated_sc_benchmarks.Rmd")'
-singularity exec ../singularity/reports_r.sif Rscript -e 'rmarkdown::render("nanopore_bulk_sc_benchmarks_hvts.Rmd")'
-singularity exec ../singularity/reports_r.sif Rscript -e 'rmarkdown::render("nanopore_bulk_sc_benchmarks_tpm.Rmd")'
-singularity exec ../singularity/reports_r.sif Rscript -e 'rmarkdown::render("nanopore_bulk_igrov_mean_rel_diff.Rmd")'
-singularity exec ../singularity/reports_r.sif Rscript -e 'rmarkdown::render("nanopore_bulk_sc_igrov_mean_rel_diff.Rmd")'
-singularity exec ../singularity/reports_r.sif Rscript -e 'rmarkdown::render("nanopore_bulk_sc_igrov_cor.Rmd")'
-singularity exec ../singularity/reports_r.sif Rscript -e 'rmarkdown::render("nanopore_sc_umap.Rmd")'
+singularity exec ../singularity/isosceles.sif Rscript -e 'rmarkdown::render("simulated_sc_benchmarks.Rmd")'
+singularity exec ../singularity/isosceles.sif Rscript -e 'rmarkdown::render("nanopore_bulk_sc_benchmarks_hvts.Rmd")'
+singularity exec ../singularity/isosceles.sif Rscript -e 'rmarkdown::render("nanopore_bulk_sc_benchmarks_tpm.Rmd")'
+singularity exec ../singularity/isosceles.sif Rscript -e 'rmarkdown::render("nanopore_bulk_igrov_mean_rel_diff.Rmd")'
+singularity exec ../singularity/isosceles.sif Rscript -e 'rmarkdown::render("nanopore_bulk_sc_igrov_mean_rel_diff.Rmd")'
+singularity exec ../singularity/isosceles.sif Rscript -e 'rmarkdown::render("nanopore_bulk_sc_igrov_cor.Rmd")'
+singularity exec ../singularity/isosceles.sif Rscript -e 'rmarkdown::render("nanopore_sc_umap.Rmd")'
