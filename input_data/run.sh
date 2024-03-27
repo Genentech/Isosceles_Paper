9ec9d86065cc6024d863f568571f2fe87dbf2f9e
Michal Kabza <michal.kabza@contractors.roche.com>
2024-03-26 17:15:28 -0700

    Updated the input/ directory

--- input_data/run.sh
+++ input_data/run.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
-# Download Promethion bulk RNA-Seq FASTQ files from NCBI (GEO SuperSeries GSE248118),
-# then save them in the fastq_ont/ directory
+# Download Promethion bulk RNA-Seq FASTQ files from NCBI (GEO SuperSeries GSE248118)
 fastq-dump --gzip SRR26865809
 mv SRR26865809.fastq.gz fastq_ont/LIB5432309_SAM24385452.fastq.gz
 fastq-dump --gzip SRR26865808
@@ -19,28 +18,29 @@ mv SRR26865803.fastq.gz fastq_ont/LIB5432315_SAM24385458.fastq.gz
 fastq-dump --gzip SRR26865802
 mv SRR26865802.fastq.gz fastq_ont/LIB5432316_SAM24385459.fastq.gz
 
-# Download MinION bulk RNA-Seq FASTQ files from NCBI (GEO SuperSeries GSE248118),
-# then save them in the fastq_ont/ directory
+# Download MinION bulk RNA-Seq FASTQ files from NCBI (GEO SuperSeries GSE248118)
 fastq-dump --gzip SRR26865801
 mv SRR26865801.fastq.gz fastq_ont/LIB5427896_SAM24376275.fastq.gz
 fastq-dump --gzip SRR26865800
 mv SRR26865800.fastq.gz fastq_ont/LIB5427897_SAM24376276.fastq.gz
 
-# Download Nanopore scRNA-Seq FASTQ file from NCBI (GEO SuperSeries GSE248118),
-# then save it in the fastq_ont/ directory
+# Download Nanopore scRNA-Seq FASTQ file from NCBI (GEO SuperSeries GSE248118)
 fastq-dump --gzip SRR26865982
 mv SRR26865982.fastq.gz fastq_ont/LIB5445493_SAM24404003.fastq.gz
 
-# Download Illumina scRNA-Seq BAM file from NCBI (GEO SuperSeries GSE248118),
-# then save it in the cellranger/ directory
+# Download Illumina scRNA-Seq BAM file from NCBI (GEO SuperSeries GSE248118)
 wget -O cellranger/possorted_genome_bam.bam \
   https://sra-pub-src-1.s3.amazonaws.com/SRR26865983/LIB5445371_SAM24404003.bam.1
 samtools index cellranger/possorted_genome_bam.bam
 
 # Download simulated Nanopore FASTQ files
+cd fastq_sim
 wget https://zenodo.org/record/8180696/files/truncated_bulk_rnaseq.fastq.gz
 wget https://zenodo.org/record/8180696/files/truncated_scrnaseq.fastq.gz
+cd ..
 
 # Download the simulated Nanopore scRNA-Seq BAM file
+cd bam_sim
 wget https://zenodo.org/record/8180696/files/truncated_scrnaseq.bam
-samtools index bam_sim/truncated_scrnaseq.bam
+samtools index truncated_scrnaseq.bam
+cd ..
