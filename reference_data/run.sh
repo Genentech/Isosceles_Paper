0c7b87874eeb4ab22ed35df516b6d5acb3038dcd
Michal Kabza <michal.kabza@contractors.roche.com>
2024-04-02 19:07:15 -0700

    Updated the Zenodo download links

--- reference_data/run.sh
+++ reference_data/run.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
 # Download reference genome and annotation data
-wget https://zenodo.org/record/8180605/files/reference_data.tgz
+wget https://zenodo.org/record/10910103/files/reference_data.tgz
 tar xfz reference_data.tgz
 rm -f reference_data.tgz
--- simulated_ovarian_analysis/download_report_data.sh
+++ simulated_ovarian_analysis/download_report_data.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
 # Download the already prepared report data
-wget https://zenodo.org/record/0000000/files/report_data_simulated_ovarian_analysis.tgz
+wget https://zenodo.org/record/10910103/files/report_data_simulated_ovarian_analysis.tgz
 tar xfz report_data_simulated_ovarian_analysis.tgz
 rm -f report_data_simulated_ovarian_analysis.tgz
